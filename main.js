d5f1dddaf80a08dd47cea8f66a57f680c967148f
Ahaan18 <71545644+Ahaan18@users.noreply.github.com>
2021-01-03 14:09:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var menu_list_array = ["Margarita Pizza","Deluxe Veggie Pizza","Paneer Tikka Pizza","Veg Supreme Pizza","Pepproni Pizza"]
+function getmenu(){
+    var htmldata;
+    htmldata = "<ol class='menulist'>";
+    menu_list_array.sort()
+    for(var i=0;i<menu_list_array;i++){
+        htmldata=htmldata+"<li>" + menu_list_array[i] + "</li>"
+    }
+    htmldata=htmldata + "</ol>"
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
